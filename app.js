816cd9e9b652e3107a4fdd6071b16db8b02e5ce4
anishazahan <anishazahan13@gmail.com>
2022-10-18 15:29:19 +0600

    mvc architecture add

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+////,,,,middle ware,,,,
+
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -9,6 +11,8 @@ app.use(express.json());
 app.get ("/",(req,res)=>{
     res.sendFile(__dirname + "/./views/index.html")
 })
+
+
 
 
 module.exports = app;
