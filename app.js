9b814b8c1bbf0ca646a9eb61a23ff486695ad18d
anishazahan <anishazahan13@gmail.com>
2022-10-18 15:41:40 +0600

    server error handling

--- app.js
+++ app.js
@@ -8,14 +8,22 @@ app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+
+
 app.get ("/",(req,res)=>{
     res.sendFile(__dirname + "/./views/index.html")
 })
+
+
 app.use ((req,res,next)=>{
     res.status(404).json({
         "message":"Route Not Found"
     });
 })
+
+
+
+///---------server error handling------------
 app.use ((err,req,res,next)=>{
     res.status(5000).json({
         "message":"Something broke"
