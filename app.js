237a2af34d5017f01987e405e67bab978eae2620
anishazahan <anishazahan13@gmail.com>
2022-10-18 15:33:22 +0600

    initial server running

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.get ("/",(req,res)=>{
     res.sendFile(__dirname + "/./views/index.html")
 })
+app.use ((req,res,next)=>{
+    res.status(404).json({
+        "message":"Route Not Found"
+    });
+})
 
 
 
