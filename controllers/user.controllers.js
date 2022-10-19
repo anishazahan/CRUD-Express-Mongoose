33625e8c384fca375c0e223ecd88e50ca67c7eeb
anishazahan <anishazahan13@gmail.com>
2022-10-19 16:05:45 +0600

    function make for all user

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -5,5 +5,29 @@ const getAllUsers = (req,res)=>{
         message:"all users"
     });
 }
+const getOneUsers = (req,res)=>{
+    res.status(200).json({
+        message:"get one users"
+    });
+}
+const createUser = (req,res)=>{
+    res.status(201).json({
+        message:"Create users"
+    });
+}
+const updateUser = (req,res)=>{
+    res.status(200).json({
+        message:"update users"
+    });
+}
+const deleteUser = (req,res)=>{
+    res.status(200).json({
+        message:"update users"
+    });
+}
 
-module.exports = getAllUsers;
+module.exports = {
+    getAllUsers,
+    getOneUsers,
+    createUser
+};
