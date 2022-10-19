1bd3c68d8ba090665125629dbaa0f6b07ca547fd
anishazahan <anishazahan13@gmail.com>
2022-10-19 20:40:54 +0600

    uuid install

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,26 +1,41 @@
 
+const User = require('../models/user.model')
 
 
+//   api/user : GET
 const getAllUsers = (req,res)=>{
     res.status(200).json({
         message:"all users"
     });
 }
+
+//   api/user/:id : GET
 const getOneUsers = (req,res)=>{
     res.status(200).json({
         message:"get one users"
     });
 }
+
+//   api/user/ : POST
 const createUser = (req,res)=>{
+    const newUser = new User({
+        id:
+        name:req.body.name,
+        age:Number(req.body.name)
+    })
     res.status(201).json({
         message:"Create users"
     });
 }
+
+//   api/user/:id : PATCH
 const updateUser = (req,res)=>{
     res.status(200).json({
         message:"update users"
     });
 }
+
+//   api/user/:id : DELETE
 const deleteUser = (req,res)=>{
     res.status(200).json({
         message:"update users"
